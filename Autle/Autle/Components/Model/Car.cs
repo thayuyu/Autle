fabd2ccd447d21971b69fdec9076679f4a45abf7
Yunes El Hamad <yunes_elhamad@sluz.ch>
2024-04-26 13:58:27 +0200

    Added first function: GetRandomCar and added a table

---
+++ Autle/Autle/Components/Model/Car.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Security.Cryptography.Xml;
+
+namespace Autle.Components.Model
+{
+    public class Car
+    {
+        public int CID { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string CarType { get; set; }
+        public int HorsePwr { get; set; }
+        public string Engine { get; set; }
+        public int Doors { get; set; }
+        public int ModelYear { get; set; }
+        public int ZeroToHundred { get; set; }
+
+        public Car(int cid, string name, string manufacturer, string cartype, int horsepwr, string engine, int doors, int modelyear, int zerotohundred)
+        {
+            CID = cid;
+            Name = name;
+            Manufacturer = manufacturer;
+            CarType = cartype;
+            HorsePwr = horsepwr;
+            Engine = engine;
+            Doors = doors;
+            ModelYear = modelyear;
+            ZeroToHundred = zerotohundred;
+        }
+    }
+}
