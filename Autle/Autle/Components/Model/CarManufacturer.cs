addf22e3862b7afbab3ad93201b8c6c7bd2e20a8
Yunes El Hamad <yunes_elhamad@sluz.ch>
2024-05-10 19:57:31 +0200

    started with the third gammode

---
+++ Autle/Autle/Components/Model/CarManufacturer.cs
@@ -0,0 +1,22 @@
+﻿namespace Autle.Components.Model
+{
+    public class CarManufacturer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PathStage1 { get; set; }
+        // TODO: How many pictures per Brand?
+
+        public CarManufacturer(int id, string name, string path1) 
+        {
+            ID = id;
+            Name = name;
+            PathStage1 = path1;
+        }
+
+        public CarManufacturer(string name)
+        {
+            Name = name;
+        }
+    }
+}
