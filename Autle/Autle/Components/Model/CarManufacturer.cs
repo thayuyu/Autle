d52594be17f7a0696e716a9aca183a89ab213f7a
Yunes El Hamad <yunes_elhamad@sluz.ch>
2024-05-11 18:51:43 +0200

    almost finished 3rd gamemode, needs fixing a bug with amount of guesses, added pictures just for testing

--- Autle/Autle/Components/Model/CarManufacturer.cs
+++ Autle/Autle/Components/Model/CarManufacturer.cs
@@ -5,13 +5,17 @@
         public int ID { get; set; }
         public string Name { get; set; }
         public string PathStage1 { get; set; }
+        public string PathStage2 { get; set; }
+        public string PathStage3 { get; set; }
         // TODO: How many pictures per Brand?
 
-        public CarManufacturer(int id, string name, string path1) 
+        public CarManufacturer(int id, string name, string path1, string path2, string path3) 
         {
             ID = id;
             Name = name;
             PathStage1 = path1;
+            PathStage2 = path2;
+            PathStage3 = path3;
         }
 
         public CarManufacturer(string name)
